ebbb665efb517a090cf22916efafd0dbc8a6dc76
Vamsi krishna Somepalli <kavvam@Vamsis-MBP.attlocal.net>
2020-07-24 14:55:12 -0500

    Updated images

--- Color_Quantization.py
+++ Color_Quantization.py
@@ -5,16 +5,13 @@ from skimage import io
 import os
 
 
-def quantize(raster, n_colors):
-    width, height, depth = raster.shape
-    reshaped_raster = np.reshape(raster, (width * height, depth))
-
-    model = cluster.KMeans(n_clusters=n_colors)
-    labels = model.fit_predict(reshaped_raster)
+def quantize(image, cluster_param):
+    width, height, depth = image.shape
+    reshaped_image = np.reshape(image, (width * height, depth))
+    model = cluster.KMeans(n_clusters=cluster_param)
+    labels = model.fit_predict(reshaped_image)
     palette = model.cluster_centers_
-
     quantized_raster = np.reshape(palette[labels], (width, height, palette.shape[1]))
-
     return quantized_raster
 
 
@@ -24,7 +21,7 @@ def main():
                  'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image3.jpg',
                  'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image4.jpg',
                  'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image5.jpg']
-    kmeans_clusters =[8,16,32]
+    kmeans_clusters =[8,12,16]
     path = 'quantizedImages'
 
     if not os.path.exists(path):
@@ -35,8 +32,10 @@ def main():
         for cluster_param in kmeans_clusters:
             quantized_raster = quantize(raster, cluster_param)
             io.imsave('./'+path+'/quantized_image_'+str(image_counter)+'_K_'+str(cluster_param)+'.png', quantized_raster);
-            print(url)
-            print(cluster_param)
+            print('image URL"'+url)
+            print('K-Value: '+ cluster_param)
         image_counter = image_counter + 1
-        sys.exit()
+    sys.exit(0)
+
+    
 main();
