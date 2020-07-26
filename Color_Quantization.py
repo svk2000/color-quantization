ce09b29e6c3ad7416c5aa8d40622f0ae79fed5f4
Vamsi krishna Somepalli <kavvam@Vamsis-MBP.attlocal.net>
2020-07-25 20:43:40 -0500

    Updated code

--- Color_Quantization.py
+++ Color_Quantization.py
@@ -18,24 +18,23 @@ def quantize(image, cluster_param):
 def main():
     image_urls =['https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image1.jpg',
                  'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image2.jpg',
-                 'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image3.jpg',
-                 'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image4.jpg',
-                 'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image5.jpg']
-    kmeans_clusters =[8,12,16]
+                 'https://personal.utdallas.edu/~axn112530/cs6375/unsupervised/images/image3.jpg']
+    kmeans_clusters = [4,8,12]
     path = 'quantizedImages'
-
+    if len(sys.argv) > 1:
+        kmeans_clusters = [int(i) for i in list(sys.argv[1].split(','))]
     if not os.path.exists(path):
         os.makedirs(path)
     image_counter = 1
     for url in image_urls:
-        raster = io.imread(url)
+        raster = io.imread(url).astype(np.uint8)
         for cluster_param in kmeans_clusters:
             quantized_raster = quantize(raster, cluster_param)
-            io.imsave('./'+path+'/quantized_image_'+str(image_counter)+'_K_'+str(cluster_param)+'.png', quantized_raster);
-            print('image URL"'+url)
-            print('K-Value: '+ cluster_param)
+            io.imsave('./'+path+'/quantized_image_'+str(image_counter)+'_K_'+str(cluster_param)+'.jpg', quantized_raster);
+            print('image URL :'+url)
+            print('K-Value: '+ str(cluster_param))
         image_counter = image_counter + 1
     sys.exit(0)
 
-    
+
 main();
